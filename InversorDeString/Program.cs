6275cbaec6263cf1ed8c62342777f971748edfb5
alexsandro49 <alexsandro49.dev@gmail.com>
2024-03-06 08:35:59 -0300

    feat: InversorDeString completed

--- InversorDeString/Program.cs
+++ InversorDeString/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Inversor de Strings");
+Console.Write("Por favor, informe uma string: ");
+string? stringOriginal = Console.ReadLine();
+string stringInvertida = "";
+
+if (!(stringOriginal is null))
+{
+    for (int i = stringOriginal.Length - 1; i >= 0; i--)
+    {
+        stringInvertida += stringOriginal[i];
+    }
+}
+
+
+Console.WriteLine("=-=-=-=-=-=-=-=-=-=");
+Console.WriteLine($"STRING ORIGINAL: {stringOriginal}\nSTRING INVERTIDA: {stringInvertida}");
+Console.WriteLine("=-=-=-=-=-=-=-=-=-=");
+
