6821ed7059f5de443522fcf2663fbfae67ab4127
alexsandro49 <alexsandro49.dev@gmail.com>
2024-03-06 03:20:53 -0300

    feat: SequenciaFibonacci completed

--- SequenciaFibonacci/Program.cs
+++ SequenciaFibonacci/Program.cs
@@ -1,2 +1,44 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Sequência Fibonacci");
+
+int numeroEscolhido = 49;
+int quantidadeDeElementos = 50;
+
+uint valorA = 0;
+uint valorB = 1;
+uint valorC = 0;
+bool elementoEncontrado = false;
+
+
+Console.WriteLine("=-=-=-=-=-=-=-=-=-=");
+Console.Write($"{valorA} - ");
+for (int i = 0; i < quantidadeDeElementos; i++)
+{
+    valorA = valorB;
+    valorB = valorC;
+    valorC = valorA + valorB;
+    int ultimoElementoDaSequencia = quantidadeDeElementos - 1;
+
+    if (i == ultimoElementoDaSequencia)
+    {
+        Console.WriteLine(valorC);
+    }
+    else
+    {
+        Console.Write($"{valorC} - ");
+    }
+
+    if (numeroEscolhido == 0 || numeroEscolhido == 1 || numeroEscolhido == i)
+        elementoEncontrado = true;
+}
+Console.WriteLine("=-=-=-=-=-=-=-=-=-=");
+
+
+if (elementoEncontrado)
+{
+    Console.WriteLine($"O número {numeroEscolhido} pertence a sequência de Fibonacci.");
+}
+else
+{
+    Console.WriteLine($"O número {numeroEscolhido} não pertence a sequência de Fibonacci.");
+}
+
